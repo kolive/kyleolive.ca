0ed551fb9f3748807d48a7efa6483c5ef0479c4b
Kyle Olive <kolive@uwaterloo.ca>
2014-10-26 18:37:48 -0400

    initial under construction commit

--- index.js
+++ index.js
@@ -2,12 +2,33 @@ var express = require('express')
 var app = express();
 var jade = require('jade');
 
-var homepage = jade.compileFile('./templates/home.jade');
+var app_path = '/home/kolive/staging.kyleolive.ca';
 
-app.use('/stylesheets', express.static(__dirname + '/static/stylesheets/'));
+var homepage = jade.compileFile(app_path + '/templates/home.jade');
+
+app.use('/stylesheets', express.static(app_path + '/static/stylesheets/'));
+app.use('/images', express.static(app_path + '/static/images/'));
+
 
 app.get('/', function(req, res) {
-  res.send(homepage({pageTitle : 'Hello World', latestPostTitle : 'Welcome to the world!' }));
+  res.send(homepage({
+    pageTitle : 'Two Bit Repo', 
+    latestPostTitle : "Sorry! My site is currently being overhauled.",
+    highlightBackground: 'background-image: url(/images/highlight.png)'}));
+});
+
+app.get('*', function(req, res, next) {
+    var err = new Error();
+      err.status = 404;
+        next(err);
+});
+
+app.use(function(err, req, res, next){
+  if(err.status !== 404){
+    return next();
+  }
+
+  res.redirect('/');
 });
 
 
