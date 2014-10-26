5991770cb674da2b8d407886a39c98090cd262c2
Kyle Olive <kolive@uwaterloo.ca>
2014-10-25 22:37:10 -0400

    jade and static css serving

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 var express = require('express')
 var app = express();
+var jade = require('jade');
+
+var homepage = jade.compileFile('./templates/home.jade');
+
+app.use('/stylesheets', express.static(__dirname + '/static/stylesheets/'));
 
 app.get('/', function(req, res) {
-  res.send('Hello World!');
+  res.send(homepage({pageTitle : 'Hello World', latestPostTitle : 'Welcome to the world!' }));
 });
+
 
 exports.app = app;
